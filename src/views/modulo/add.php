a106936229beb5183f56a4d2c8398f3728cce741
kairiroberto <kairiroberto@gmail.com>
2025-06-12 12:25:34 -0300

    Update add.php

--- src/views/modulo/add.php
+++ src/views/modulo/add.php
@@ -1,9 +1,20 @@
-<h1>➕ Novo Módulo</h1>
-<form method="post">
-    <?= \yii\helpers\Html::hiddenInput(Yii::$app->request->csrfParam, Yii::$app->request->getCsrfToken()) ?>
-    <label>Nome do Módulo:</label><br>
-    <input type="text" name="nome" required><br><br>
-    <label>Classe do Módulo (ex: <code>app\\modules\\teste\\Module</code>):</label><br>
-    <input type="text" name="classe" required><br><br>
-    <button type="submit">Salvar</button>
-</form>
+<?php
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = '➕ Novo Módulo';
+?>
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<?php $form = ActiveForm::begin(); ?>
+
+<?= $form->field($model, 'nome')->textInput(['placeholder' => 'Nome do Módulo'])->label('Nome do Módulo:') ?>
+
+<?= $form->field($model, 'classe')->textInput(['placeholder' => 'Ex: app\\modules\\teste\\Module'])->label('Classe do Módulo:') ?>
+
+<div class="form-group">
+    <?= Html::submitButton('Salvar', ['class' => 'btn btn-primary']) ?>
+</div>
+
+<?php ActiveForm::end(); ?>
