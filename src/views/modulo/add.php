639525e3a923fea27e2ffe7cdc83e591fd9a211d
kairiroberto <kairiroberto@gmail.com>
2025-06-15 22:14:59 -0300

    Update add.php

--- src/views/modulo/add.php
+++ src/views/modulo/add.php
@@ -9,12 +9,31 @@ $this->title = '➕ Novo Módulo';
 
 <?php $form = ActiveForm::begin(); ?>
 
-<?= $form->field($model, 'nome')->textInput(['placeholder' => 'Nome do Módulo'])->label('Nome do Módulo:') ?>
+<?= $form->field($model, 'nome')
+    ->textInput(['id' => 'input-nome', 'placeholder' => 'Nome do Módulo'])
+    ->label('Nome do Módulo:') ?>
 
-<?= $form->field($model, 'classe')->textInput(['placeholder' => 'Ex: app\\modules\\teste\\Module'])->label('Classe do Módulo:') ?>
+<?= $form->field($model, 'classe')
+    ->textInput(['id' => 'input-classe', 'placeholder' => 'Ex: app\\modules\\teste\\Module'])
+    ->label('Classe do Módulo: (app\\modules\\NOME\\Module)') ?>
 
 <div class="form-group">
     <?= Html::submitButton('Salvar', ['class' => 'btn btn-primary']) ?>
 </div>
 
 <?php ActiveForm::end(); ?>
+
+<?php
+$js = <<<JS
+document.getElementById('input-nome').addEventListener('input', function() {
+    const nome = this.value.trim().toLowerCase();
+    if(nome) {
+        const classe = 'app\\\\modules\\\\' + nome + '\\\\Module';
+        document.getElementById('input-classe').value = classe;
+    } else {
+        document.getElementById('input-classe').value = '';
+    }
+});
+JS;
+$this->registerJs($js);
+?>
